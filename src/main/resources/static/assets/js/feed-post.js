72faae8823adf0f5027ed66be265f19bf030a1ba
dazzlingtea <kitsnr08@gmail.com>
2024-06-23 00:09:03 +0900

    # feed 이미지 업로드 - fetch

--- src/main/resources/static/assets/js/feed-post.js
+++ src/main/resources/static/assets/js/feed-post.js
@@ -1,38 +1,55 @@
 import {FEED_URL} from "./feed-list.js";
-import {previewImages, handleFileInputChange} from "./image.js";
+import {dataToFormData, handleFileInputChange} from "./image.js";
 
-// 모달 작성 완료 버튼 클릭 시 이벤트
 const $feedPostBtn = document.getElementById('feed-post-Btn')
 const $imageInput = document.getElementById('postImage');
 const $imageBox = document.querySelector('.dropbox');
+let imageFiles = [];
 
+// 이미지 input 변경 시 발생 이벤트
+$imageInput.addEventListener('change', e => {
+    imageFiles = handleFileInputChange(e, imageFiles, $imageBox);
+});
+
+// 미리보기 확인 후 fetch
+const fetchFeedPost = async (payload) => {
+    try {
+        const res
+            = await fetch(FEED_URL + '/list', payload);
+        if (response.ok) {
+            const result = await response.json();
+            alert('게시글이 성공적으로 등록되었습니다.');
+            document.getElementById('createFeedModal').style.display = 'none';
+            // formData = new FormData(); // FormData 초기화
+            imageFiles = []; // 파일 리스트 초기화
+            document.getElementById('createFeedForm').reset();
+            document.querySelector('.dropbox').innerHTML = '';
+        } else {
+            throw new Error('서버 응답이 올바르지 않습니다.');
+        }
+    } catch (e) {
+        console.log('fetchFeedPost 에러: ', error);
+    }
+};
+
+// 모달 작성 완료 버튼 클릭 시 이벤트
 $feedPostBtn.addEventListener('click', e => {
     e.preventDefault();
 
     // 태그들 value, 이미지 파일명 가져오기
     // 로그인한 회원 정보 가져오기
-    const $createContent = document.getElementById('cr-content');
-    const $createImages = document.querySelector('#createFeedModal ')
+    const feedContent = document.getElementById('cr-content').value;
+
+    // 입력값 누락 있는지 확인 필요
 
     // fetch payload에 담아서 POST 요청
-
-});
-
-$imageInput.addEventListener('change', e => {
-    const files = e.target.files;
-    previewImages(files, $imageBox);
-});
-
-const getImageFile = e => {
-    const files = e.target.files; // 유사배열객체
-    const reader = new FileReader();
-
-    setImageFile(Array.from(files)); // 이미지 파일 자체
-
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-        setPreviewFile(reader.result); // 미리보기
+    const formData = dataToFormData({content}, imageFiles);
+    const payload = {
+        method: 'POST',
+        body: formData
     };
-};
-// 미리보기 확인 후 fetch
-// const fetch
+
+    fetchFeedPost(payload);
+
+
+});
--- src/main/resources/static/assets/js/image.js
+++ src/main/resources/static/assets/js/image.js
@@ -1,7 +1,6 @@
 // 이미지
 
 
-
 // 미리보기 이미지 렌더링 함수
 // imageBox는 미리보기 이미지가 렌더링될 DOM
 export function previewImages(files, imageBox) {
@@ -18,16 +17,19 @@ export function previewImages(files, imageBox) {
             // img.style.height = '100px';
             imageBox.innerHTML += imgTag;
         };
+        // 파일을 Data URL로 읽기 완료하면 reader.onload 콜백함수 실행
         reader.readAsDataURL(file);
     });
 }
 
 // 이미지 input(e) 변경 시 미리보기 및 이미지 배열 생성
 // dropbox : 미리보기 렌더링 될 부모 태그
-export function handleFileInputChange(e, dropbox, maxFiles=10) {
-    const files = Array.from(e.target.files).slice(0, maxFiles);
-    previewImages(files, dropbox);
-    return files;
+export function handleFileInputChange(e, imageFiles, imageBox) {
+    const newFiles = Array.from(e.target.files);
+    imageFiles.push(...newFiles);
+    previewImages(imageFiles, imageBox);
+    e.target.value = '';
+    return imageFiles;
 }
 
 // 게시글 작성, 수정(이미지 포함) FormData에 담는 함수
