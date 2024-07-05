472c7da6fb4c0e3e5253b5365174dbdec7f73c62
SoobihnKim <ksbsj92@naver.com>
2024-07-05 17:26:15 +0900

    # MypageBoardController 추가

---
+++ src/main/java/com/travel/project/controller/MypageBoardController.java
@@ -0,0 +1,35 @@
+package com.travel.project.controller;
+
+import com.travel.project.dto.response.AccBoardListDto;
+import com.travel.project.service.MypageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class MypageBoardController {
+
+    private final MypageService mypageService;
+
+    @GetMapping("/mypage/boards/{account}")
+    public ResponseEntity<List<AccBoardListDto>> getboards(@PathVariable String account, HttpSession session) {
+
+        List<AccBoardListDto> boardsByAccount = mypageService.findBoardsByAccount(session);
+//        Search search = new Search();
+//        search.setAccount(account);
+//        MyBoardListDto myBoardList = mypageService.findBoardsByAccount(session);
+//        List<AccBoardListDto> boardList = myBoardListDto.getBoards();
+
+        log.debug("boardsByAccount: {}", boardsByAccount);
+
+        return ResponseEntity.ok().body(boardsByAccount);
+    }
+}
